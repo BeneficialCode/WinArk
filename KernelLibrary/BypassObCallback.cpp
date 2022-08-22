5e4e982e7ff8030a56f82e8960d8fe6bc99962f7
CORP\zengquan <3123132899@qq.com>
2022-08-22 18:32:02 +0800

    Added ObjectCallback bypass

--- KernelLibrary/BypassObCallback.cpp
+++ KernelLibrary/BypassObCallback.cpp
@@ -1,3 +1,46 @@
 #include "pch.h"
 #include "BypassObCallback.h"
+#include "detours.h"
 
+PObpCallPreOperationCallbacks g_pObpCallPreOperationCallbacks = nullptr;
+
+extern "C" {
+	NTKERNELAPI UCHAR* NTAPI PsGetProcessImageFileName(_In_ PEPROCESS Process);
+}
+
+NTSTATUS NTAPI
+ObpCallPreOperationCallbacks(POBJECT_TYPE ObjectType,
+	POB_PRE_OPERATION_INFORMATION Info,
+	PLIST_ENTRY PostCallbackListHead) {
+	if (!strcmp((char*)PsGetProcessImageFileName(PsGetCurrentProcess()), "WinArk")) {
+		return STATUS_SUCCESS;
+	}
+	return g_pObpCallPreOperationCallbacks(ObjectType, Info, PostCallbackListHead);
+}
+
+bool BypassObjectCallback::Bypass(void* address) {
+	g_pObpCallPreOperationCallbacks = (PObpCallPreOperationCallbacks)address;
+	if (g_pObpCallPreOperationCallbacks == nullptr)
+		return false;
+	NTSTATUS status = DetourAttach((PVOID*)&g_pObpCallPreOperationCallbacks, ObpCallPreOperationCallbacks);
+	if (!NT_SUCCESS(status))
+		return false;
+
+	status = DetourTransactionCommit();
+	if (!NT_SUCCESS(status))
+		return false;
+
+	return true;
+}
+
+bool BypassObjectCallback::Unbypass() {
+	NTSTATUS status = DetourDetach((PVOID*)&g_pObpCallPreOperationCallbacks, ObpCallPreOperationCallbacks);
+	if (!NT_SUCCESS(status))
+		return false;
+
+	status = DetourTransactionCommit();
+	if (!NT_SUCCESS(status))
+		return false;
+
+	return true;
+}
--- KernelLibrary/BypassObCallback.h
+++ KernelLibrary/BypassObCallback.h
@@ -6,3 +6,13 @@ using PObpCallPreOperationCallbacks = NTSTATUS
 	POB_PRE_OPERATION_INFORMATION Info,
 	PLIST_ENTRY PostCallbackListHead
 );
+
+NTSTATUS NTAPI
+ObpCallPreOperationCallbacks(POBJECT_TYPE ObjectType,
+	POB_PRE_OPERATION_INFORMATION Info,
+	PLIST_ENTRY PostCallbackListHead);
+
+struct BypassObjectCallback {
+	static bool Bypass(void* address);
+	static bool Unbypass();
+};
