8eeaeaf2dd5ba88d2c38e8378945ca33f171ea73
BeneficialCode <3123132899@qq.com>
2023-06-21 15:35:53 +0800

    tweak minor bug

--- WinSysCore/NewKernelHandler.h
+++ WinSysCore/NewKernelHandler.h
@@ -5,17 +5,17 @@ struct NewKernelHandler {
 	static void InitMaxPhyMask();
 
 	// number of bits a physical address can be made up of
-	static BYTE _MaxPhyAddr;
+	static inline BYTE _MaxPhyAddr;
 	// mask to AND with a physical address to strip invalid bits
-	static ULONG_PTR _MaxPhyAddrMask{ 0xfffffffff };
+	static inline ULONG_PTR _MaxPhyAddrMask{ 0xfffffffff };
 	// same as MAXPHYADDRMASK but also aligns to strip invalid bits
-	static ULONG_PTR _MaxPhyAddrMaskPb{ 0xffffff000 };
-	static ULONG_PTR _MaxPhyAddrMaskPbBig{ 0xfffe00000 };
+	static inline ULONG_PTR _MaxPhyAddrMaskPb{ 0xffffff000 };
+	static inline ULONG_PTR _MaxPhyAddrMaskPbBig{ 0xfffe00000 };
 
 	// number of bits in a virtual address. All bits after it have to match
-	static BYTE _MaxLinearAddr;
-	static ULONG_PTR _MaxLinearAddrTest;
-	static ULONG_PTR _MaxLinearAddrMask;
+	static inline BYTE _MaxLinearAddr;
+	static inline ULONG_PTR _MaxLinearAddrTest;
+	static inline ULONG_PTR _MaxLinearAddrMask;
 
 	bool VerifyAddress(ULONG_PTR addr);
 };
