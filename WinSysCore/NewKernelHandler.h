7a124faad435448c489a85ca890766967c784cff
BeneficialCode <3123132899@qq.com>
2023-08-30 17:31:16 +0800

    fixup the compile error on x86.

--- WinSysCore/NewKernelHandler.h
+++ WinSysCore/NewKernelHandler.h
@@ -7,10 +7,10 @@ struct NewKernelHandler {
 	// number of bits a physical address can be made up of
 	static inline BYTE _MaxPhyAddr;
 	// mask to AND with a physical address to strip invalid bits
-	static inline ULONG_PTR _MaxPhyAddrMask{ 0xfffffffff };
+	static inline ULONG64 _MaxPhyAddrMask{ 0xfffffffff };
 	// same as MAXPHYADDRMASK but also aligns to strip invalid bits
-	static inline ULONG_PTR _MaxPhyAddrMaskPb{ 0xffffff000 };
-	static inline ULONG_PTR _MaxPhyAddrMaskPbBig{ 0xfffe00000 };
+	static inline ULONG64 _MaxPhyAddrMaskPb{ 0xffffff000 };
+	static inline ULONG64 _MaxPhyAddrMaskPbBig{ 0xfffe00000 };
 
 	// number of bits in a virtual address. All bits after it have to match
 	static inline BYTE _MaxLinearAddr;
