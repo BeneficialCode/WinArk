fb97c376886546417b3cbaf55966d4867c01bcc1
BeneficialCode <3123132899@qq.com>
2023-10-09 18:15:25 +0800

    Modified the comments.

--- KernelLibrary/SysMon.h
+++ KernelLibrary/SysMon.h
@@ -73,7 +73,7 @@ struct _OB_CALLBACK_BLOCK;
 
 typedef struct _OB_CALLBACK_ENTRY {
 	LIST_ENTRY CallbackList;
-	OB_OPERATION Operations;						// magic bit
+	OB_OPERATION Operations;						// 1 for Creations, 2 for Duplications
 	ULONG Flags;
 	struct _OB_CALLBACK_BLOCK* RegistrationHandle;	// Points to the OB_CALLBACK_BLOCK used for ObUnRegisterCallback
 	POBJECT_TYPE ObjectType;
