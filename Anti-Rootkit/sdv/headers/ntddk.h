2db014589d3033548dd7cc8f0fea1d7ab6f59f33
BeneficialCode <3123132899@qq.com>
2023-10-29 19:54:11 +0800

    Delete the sdv

--- Anti-Rootkit/sdv/headers/ntddk.h
+++
@@ -1,5 +0,0 @@
-// ntddk.h - SMV-SDV wrapper file that redefines macros as functions
-#define _SDV_NTDDK_CONTEXT_
-#include "C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\km\ntddk.h"
-#undef _SDV_NTDDK_CONTEXT_
-#include "C:\Program Files (x86)\Windows Kits\10\TOOLS\SDV\osmodel\generic\sdv_ntddk.h"
